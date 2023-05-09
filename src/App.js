6dc14d071262ad443a080e97dbdd9900a59375f6
tongdung13 <tong.van.dung@vinicorp.com.vn>
2023-05-09 11:08:07 +0700

    po

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RefreshApp, { RefreshHome } from './Views/refesh-view';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RefreshHome } from "./Views/refesh-view";
+import CreditCardUi from "./components/CreditCardUi";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<RefreshHome />}>
-        <Route index element={<RefreshHome />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    // (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<RefreshHome />}>
+    //       <Route index element={<RefreshHome />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    // ),
+    <div className="container mt-5">
+      <h2 className="mb-4">React Credit Card Payment UI Componenet Example</h2>
+      <CreditCardUi />
+    </div>
   );
 }
 
