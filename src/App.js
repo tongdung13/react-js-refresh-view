2aaf5c43a9353ad0076ccf639ee444f51707eb4e
tongdung13 <tong.van.dung@vinicorp.com.vn>
2023-06-22 08:57:04 +0700

    op

--- src/App.js
+++ src/App.js
@@ -3,37 +3,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import MyPdfViewer from "./components/DocPdf";
 // import RefreshApp from "./Views/refesh-view";
 // import Example from "./components/print";
-import { RefreshValue } from "./Views/refesh-view";
+// import { RefreshValue } from "./Views/refesh-view";
+import { Header } from "./Views/Header";
+import { Customers } from "./Views/Customers";
+import { ExportCSV } from "./Views/ExportCSV";
+import { ExportReactCSV } from "./Views/ExportReactCSV";
 
 function App() {
+  const customers = () => {
+    let custs = [];
+    for (let i = 0; i <= 25; i++) {
+      custs.push({
+        firstName: `first${i}`,
+        lastName: `last${i}`,
+        email: `abc${i}@gmail.com`,
+        address: `000${i} street city, ST`,
+        zipcode: `0000${i}`,
+      });
+    }
+    return custs;
+  };
+
   return (
-    // (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<RefreshHome />}>
-    //       <Route index element={<RefreshHome />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-    // ),
-    // (
-    //   <div className="container mt-5">
-    //     <h2 className="mb-4">
-    //       React Credit Card Payment UI Componenet Example
-    //     </h2>
-    //     <CreditCardUi />
-    //   </div>
-    // ),
-    // (
-    //   <div className="container mt-5">
-    //     <h2 className="mb-4">
-    //       React Js Add Class to Div On Window Scroll Example
-    //     </h2>
-    //     <Home />
-    //   </div>
-    // )
     <div className="App">
-      <RefreshValue />
+      <Header />
+      <div className="row">
+        <div className="col-md-8">
+          <h2>Customers</h2>
+        </div>
+        <div className="col-md-4 center">
+          {/* <ExportCSV
+            csvData={customers()}
+            fileName='test'
+          /> */}
+           <ExportReactCSV csvData={customers()} fileName='demo' style={{  }} />
+        </div>
+      </div>
+      <Customers customers={customers()} />
     </div>
   );
 }
